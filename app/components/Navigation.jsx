ae370676979f9e6e2b645778b42c37028dcfe524
Jordy Webb <jordy@thegrid.io>
2017-04-28 22:17:20 -0700

    fix nav bug

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -20,7 +20,7 @@ var Navigation = () => {
       <div className="top-bar-right">
         <ul className="menu">
           <li className="menu-text">
-            Created by <a href="http://www.mead.io" target="_blank">Andrew Mead</a>
+            Created by <a href="http://jordy.help" target="_blank">Jordy Webb</a>
           </li>
         </ul>
       </div>
