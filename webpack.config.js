32af3686e942cbf8bf9d2a1355a685d83234baf5
Jordy Webb <jordy@thegrid.io>
2017-04-29 20:30:36 -0700

    support foundation settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
   entry: [
@@ -44,6 +45,12 @@ module.exports = {
         exclude: /(node_modules|bower_components)/
       }
     ]
+  },
+  sassLoader: {
+    includePaths: [
+      path.resolve(__dirname, './node_modules/foundation-sites/scss')
+    ]
+
   },
   devtool: 'cheap-module-eval-source-map'
 };
