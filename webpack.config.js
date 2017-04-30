10b15d8fd7afe6e63d9a1adced79915da1473165
Jordy Webb <jordy@thegrid.io>
2017-04-30 03:29:15 -0700

    init commit

--- webpack.config.js
+++ webpack.config.js
@@ -23,14 +23,7 @@ module.exports = {
   resolve: {
     root: __dirname,
     alias: {
-      Main: 'app/components/Main.jsx',
-      applicationStyles: 'app/styles/app.scss',
-      Navigation: 'app/components/Navigation.jsx',
-      Timer: 'app/components/Timer.jsx',
-      Countdown: 'app/components/Countdown.jsx',
-      Clock: 'app/components/Clock.jsx',
-      Controls: 'app/components/Controls.jsx',
-      CountdownForm: 'app/components/CountdownForm.jsx'
+      applicationStyles: 'app/styles/app.scss'
     },
     extensions: ['', '.js', '.jsx']
   },
