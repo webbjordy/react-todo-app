bfc7b688beb436967214e2c8fed04b153b68811c
Jordy Webb <jordy@thegrid.io>
2017-04-30 05:05:40 -0700

    add app comoonent

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,10 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
+var TodoApp = require('TodoApp');
+
+
+
 
 // Load foundation
 require('style!css!foundation-sites/dist/foundation.min.css')
@@ -11,6 +15,6 @@ $(document).foundation();
 require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
-<p>Boilerplate project</p>,
+<TodoApp />,
   document.getElementById('app')
 );
