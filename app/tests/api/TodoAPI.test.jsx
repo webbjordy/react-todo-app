a5a2419d3001dabd6334b74bd73a1d05e3d6e907
Jordy Webb <jordy@thegrid.io>
2017-05-05 06:19:27 -0700

    add showCompleted filter and tests

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -10,7 +10,6 @@ describe('API', () => {
     expect(TodoAPI).toExist;
   });
 
-
 describe('setTodos', () => {
   it('should set valid todos array', () => {
     var todos = [{
@@ -46,6 +45,32 @@ describe('getTodos', () => {
     localStorage.setItem('todos', JSON.stringify(todos));
     var actualTodos = TodoAPI.getTodos();
     expect(actualTodos).toEqual(todos);
+  });
+});
+
+describe('filterTodos', () => {
+  var todos = [{
+    id: 1,
+    text: 'some text',
+    completed: true
+  }, {
+    id: 2,
+    text: 'some other text',
+    completed: false
+  }, {
+    id: 3,
+    text: 'some more text',
+    completed: true
+  }];
+
+  it('should return all items when showCompleted is true', () => {
+    var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+    expect(filteredTodos.length).toBe(3);
+  });
+
+  it('should only return completed items when showCompleted is false', () => {
+    var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+    expect(filteredTodos.length).toBe(1);
   });
 
 });
