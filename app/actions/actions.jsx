e66d3b797a6fd4336f0c65bfb56dfd01255235e3
Jordy Webb <jordy@thegrid.io>
2017-06-06 04:42:44 -0700

    add addTodo action

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -2,5 +2,12 @@ export var setSearchText = (searchText) => {
   return {
     type: 'SET_SEARCH_TEXT',
     searchText
-  }
+  };
+};
+
+export var addTodo = (text) => {
+  return {
+    type: 'ADD_TODO',
+    text
+  };
 };
