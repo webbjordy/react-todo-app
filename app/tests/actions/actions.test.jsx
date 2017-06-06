90974e160baaa68f43daa89940704fc1955eedf4
Jordy Webb <jordy@thegrid.io>
2017-06-05 17:39:48 -0700

    add searchText action test

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -1,1 +1,13 @@
+var expect = require('expect');
+var actions = require('actions');
 
+describe('Actions', () => {
+  it('should generate searchText action', () => {
+    var action = {
+      type: 'SET_SEARCH_TEXT',
+      searchText: 'some searchText'
+    };
+    var res = actions.setSearchText(action.searchText);
+    expect(res).toEqual(action);
+  });
+});
