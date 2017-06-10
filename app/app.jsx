bdf6ef3baf13eae1caec14cf9b695f26609d7162
Jordy Webb <jordy@thegrid.io>
2017-06-10 06:23:01 -0700

    add subscribe call

--- app/app.jsx
+++ app/app.jsx
@@ -7,6 +7,13 @@ var TodoApp = require('TodoApp');
 var actions = require('actions');
 var store = require('configureStore').configure();
 
+store.subscribe(() => {
+  console.log('New state', store.getState());
+});
+
+store.dispatch(actions.addTodo('Clean the yard'));
+store.dispatch(actions.setSearchText('yard'));
+store.dispatch(actions.toggleShowCompleted());
 
 
 // Load foundation
